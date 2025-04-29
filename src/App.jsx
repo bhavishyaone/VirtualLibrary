fcaf51fcad2b67719c1fb16417ba95e8aff953f9
Bhavishya Sharma <bhavishaya32@gmail.com>
2025-04-29 22:53:04 +0530

    page removed

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route, Link } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import BrowseBooks from './pages/BrowseBooks';
 import BookDetail from './pages/BookDetail';
-import LoginPage from './pages/LoginPage';
+;
 import SignUpPage from './pages/SignUpPage';
 import FavoritesPage from './pages/FavoritesPage';
 import { FavoritesProvider } from './context/FavoritesContext';
@@ -23,7 +23,7 @@ function App() {
                 Favorites
                 <span className="favorite-icon">❤️</span>
               </Link>
-              <Link to="/login" className="nav-link login-button">Login</Link>
+              {/* <Link to="/login" className="nav-link login-button">Login</Link> */}
             </nav>
           </div>
         </header>
@@ -33,7 +33,7 @@ function App() {
             <Route path="/" element={<HomePage />} />
             <Route path="/browse" element={<BrowseBooks />} />
             <Route path="/book/:id" element={<BookDetail />} />
-            <Route path="/login" element={<LoginPage />} />
+            {/* <Route path="/login" element={<LoginPage />} /> */}
             <Route path="/signup" element={<SignUpPage />} />
             <Route path="/favorites" element={<FavoritesPage />} />
           </Routes>
